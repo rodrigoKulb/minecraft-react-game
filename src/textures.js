ba279e286bc7b4102c28156a71936e8782625f4e
rodrigoKulb <kulb.rodrigo@gmail.com>
2021-03-14 20:52:46 -0300

    correcao de textura

--- src/textures.js
+++ src/textures.js
@@ -1,6 +1,5 @@
 import dirtImg from './images/dirt.jpg';
 import grassImg from './images/grass.jpg';
-import glassImg from './images/glass.png';
 import logImg from './images/log.jpg';
 import woodImg from './images/wood.png';
 import dirtgrassImg from './images/dirtgrass.jpg';
