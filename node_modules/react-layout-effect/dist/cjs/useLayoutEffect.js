ba90debb79c343bd4ff775d9aeffd8673056c54b
Rodrigo Kulb <rodrigo.kulb@varapido.com.br>
2022-05-19 16:43:38 -0300

    Delete node_modules directory

--- node_modules/react-layout-effect/dist/cjs/useLayoutEffect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-exports.useLayoutEffect = typeof window !== 'undefined' &&
-    window.document &&
-    window.document.createElement
-    ? React.useLayoutEffect
-    : React.useEffect;
